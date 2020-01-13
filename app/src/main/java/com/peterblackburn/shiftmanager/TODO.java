2d86399e48ae8573366c5d8aa1e10599e14e90d3
Peter Blackburn <peterblackburn86@gmail.com>
2020-01-13 01:29:43 +0000

    Updated TODO list

--- app/src/main/java/com/peterblackburn/shiftmanager/TODO.java
+++ app/src/main/java/com/peterblackburn/shiftmanager/TODO.java
@@ -2,21 +2,22 @@ package com.peterblackburn.shiftmanager;
 
 public class TODO {
     /**
-     * IMPLEMENT BASIC CALENDAR VIEW FOR DISPLAYING SHIFTS                      COMPLETE
-     * IMPLEMENT BASIC EVENT LIST                                               COMPLETE
-     * IMPLEMENT INDIVIDUAL EVENT VIEWING WITH DETAILS                          INCOMPLETE
-     * IMPLEMENT EVENT LIST SORTING / FILTERING                                 INCOMPLETE
-     * IMPLEMENT PROPER SHIFT HANDLING, WITH DATE SORTING                       PARTIALLY COMPLETE
-     * IMPLEMENT SHIFT TEMPLATES FOR FAST ADDING                                INCOMPLETE
-     * IMPLEMENT ADDING OF SHIFTS / TEMPLATES                                   PARTIALLY COMPLETE
-     * IMPLEMENT REMOVING EXISTING SHIFTS / TEMPLATES                           PARTIALLY COMPLETE
-     * IMPLEMENT BASIC USER PROFILES                                            INCOMPLETE
-     * IMPLEMENT CUSTOMISABLE PAY STRUCTURE PER USER                            INCOMPLETE
-     * IMPLEMENT CALENDAR / APP SETTINGS PER USER                               INCOMPLETE
-     * IMPLEMENT REALM                                                          MOSTLY COMPLETE
+     * IMPORTANT TASKS
      *
-     * MAKE THE APP LOOK PRETTY                                                 PARTIALLY COMPLETE
+     * VIEW EVENTS IN MORE DETAIL
+     * ALLOW EDITING OF EXISTING EVENTS
+     * EVENT FILTERING AND SORTING
+     * FULLY IMPLEMENT REALM (IN PROGRESS)
+     * USER PROFILES (APP SETTINGS / THEME SETTINGS / PERSONAL SHIFT + PAY SETTINGS)
+     * PAYSLIP ESTIMATION
+     */
+
+    /**
+     * SECONDARY TASKS
      *
-     * IMPLEMENT THEMES FOR THE APP (DARK, LIGHT ETC)                           INCOMPLETE
+     * MAKE THE APP LOOK PRETTY (IN PROGRESS)
+     * IMPLEMENT THEME ENGINE
+     * IMPLEMENT A FEW STANDARD THEMES FOR THE ENGINE (LIGHT, DARK ETC)
+     * SHIFT TEMPLATES FOR FASTING ADDING
      */
 }
